8f23a588dc943b23b2e5f58f47919307f5c392fc
floo420 <florian.brasseur@student.ehb.be>
2024-01-09 23:20:35 +0100

    fix

--- resources/views/layouts/layout2.blade.php
+++ resources/views/layouts/layout2.blade.php
@@ -6,6 +6,34 @@
     <title>Footer</title>
 </head>
 <body>
-    
+<div class="container">
+    <div class="row">
+      <div class="col-md-4 mb-3">
+        <h5 class="text-uppercase mb-4">About Us</h5>
+        <p>We provide top-quality rental services to meet your needs in Thailand.</p>
+      </div>
+
+      <div class="col-md-4 mb-3">
+        <h5 class="text-uppercase mb-4">Quick Links</h5>
+        <ul class="list-unstyled">
+          <li><a href="#" class="text-decoration-none text-light">Home</a></li>
+          <li><a href="#" class="text-decoration-none text-light">Rentals</a></li>
+          <li><a href="#" class="text-decoration-none text-light">About</a></li>
+          <li><a href="#" class="text-decoration-none text-light">Contact</a></li>
+        </ul>
+      </div>
+
+      <div class="col-md-4 mb-3">
+        <h5 class="text-uppercase mb-4">Contact Us</h5>
+        <p>Email: info@rentalwebsite.com</p>
+        <p>Phone: +1234567890</p>
+      </div>
+    </div>
+
+    <div class="footer-bottom text-center pt-3">
+      &copy; 2023 Rental Website | Designed by YourName
+    </div>
+  </div>
+</footer>
 </body>
 </html>
