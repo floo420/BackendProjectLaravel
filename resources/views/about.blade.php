13770986f8b4dbd993a2ebab5f6001ba6c68e6ce
floo420 <florian.brasseur@student.ehb.be>
2024-01-15 15:55:32 +0100

    commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -45,7 +45,7 @@
 
 <footer class="bg-light text-center text-lg-start mt-5">
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        &copy; 2023 Thai Condo Rentals | Designed by [Your Name]
+        &copy; 2023 Thai Condo Rentals | Designed by Florian Brasseur
     </div>
 </footer>
 @endsection
